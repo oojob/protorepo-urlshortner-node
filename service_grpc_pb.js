c22d13e5bc5e70b30169e55a0732e85aaf9a8f05
nirajgeorgian <niraj.kishore@stayology.com>
2020-04-29 03:02:51 +0530

    Auto Creation of Proto

--- service_grpc_pb.js
+++ service_grpc_pb.js
@@ -16,25 +16,25 @@ var grpc = require('grpc');
 var services_urlshortner_service_pb = require('../../services/urlshortner/service_pb.js');
 var oojob_protobuf_health_pb = require('../../oojob/protobuf/health_pb.js');
 
-function serialize_oojob_protobuf_HealthCheckRequest(arg) {
+function serialize_github_com_oojob_protobuf_HealthCheckRequest(arg) {
   if (!(arg instanceof oojob_protobuf_health_pb.HealthCheckRequest)) {
-    throw new Error('Expected argument of type oojob.protobuf.HealthCheckRequest');
+    throw new Error('Expected argument of type github.com.oojob.protobuf.HealthCheckRequest');
   }
   return new Buffer(arg.serializeBinary());
 }
 
-function deserialize_oojob_protobuf_HealthCheckRequest(buffer_arg) {
+function deserialize_github_com_oojob_protobuf_HealthCheckRequest(buffer_arg) {
   return oojob_protobuf_health_pb.HealthCheckRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_oojob_protobuf_HealthCheckResponse(arg) {
+function serialize_github_com_oojob_protobuf_HealthCheckResponse(arg) {
   if (!(arg instanceof oojob_protobuf_health_pb.HealthCheckResponse)) {
-    throw new Error('Expected argument of type oojob.protobuf.HealthCheckResponse');
+    throw new Error('Expected argument of type github.com.oojob.protobuf.HealthCheckResponse');
   }
   return new Buffer(arg.serializeBinary());
 }
 
-function deserialize_oojob_protobuf_HealthCheckResponse(buffer_arg) {
+function deserialize_github_com_oojob_protobuf_HealthCheckResponse(buffer_arg) {
   return oojob_protobuf_health_pb.HealthCheckResponse.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
@@ -79,10 +79,10 @@ var UrlShortnerServiceService = exports.UrlShortnerServiceService = {
     responseStream: false,
     requestType: oojob_protobuf_health_pb.HealthCheckRequest,
     responseType: oojob_protobuf_health_pb.HealthCheckResponse,
-    requestSerialize: serialize_oojob_protobuf_HealthCheckRequest,
-    requestDeserialize: deserialize_oojob_protobuf_HealthCheckRequest,
-    responseSerialize: serialize_oojob_protobuf_HealthCheckResponse,
-    responseDeserialize: deserialize_oojob_protobuf_HealthCheckResponse,
+    requestSerialize: serialize_github_com_oojob_protobuf_HealthCheckRequest,
+    requestDeserialize: deserialize_github_com_oojob_protobuf_HealthCheckRequest,
+    responseSerialize: serialize_github_com_oojob_protobuf_HealthCheckResponse,
+    responseDeserialize: deserialize_github_com_oojob_protobuf_HealthCheckResponse,
   },
   watch: {
     path: '/urlshortner.UrlShortnerService/Watch',
@@ -90,10 +90,10 @@ var UrlShortnerServiceService = exports.UrlShortnerServiceService = {
     responseStream: true,
     requestType: oojob_protobuf_health_pb.HealthCheckRequest,
     responseType: oojob_protobuf_health_pb.HealthCheckResponse,
-    requestSerialize: serialize_oojob_protobuf_HealthCheckRequest,
-    requestDeserialize: deserialize_oojob_protobuf_HealthCheckRequest,
-    responseSerialize: serialize_oojob_protobuf_HealthCheckResponse,
-    responseDeserialize: deserialize_oojob_protobuf_HealthCheckResponse,
+    requestSerialize: serialize_github_com_oojob_protobuf_HealthCheckRequest,
+    requestDeserialize: deserialize_github_com_oojob_protobuf_HealthCheckRequest,
+    responseSerialize: serialize_github_com_oojob_protobuf_HealthCheckResponse,
+    responseDeserialize: deserialize_github_com_oojob_protobuf_HealthCheckResponse,
   },
 };
 
